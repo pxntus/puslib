5508c5cfc8cbcf5dd0c96d413af72c54269e72e9
pxntus <pontusnyman@gmail.com>
2021-03-31 00:22:59 +0200

    Fixed some PUS service definitions.

--- puslib/services/__init__.py
+++ puslib/services/__init__.py
@@ -1,40 +1,12 @@
-from enum import Enum
-
 from .pus_001_request_verification import RequestVerification
 from .pus_003_housekeeping import Housekeeping
 from .pus_005_event_reporting import EventReporting
 from .pus_008_function_management import FunctionManagement
 from .pus_017_test import Test
 
-
-class PusServiceType(bytes, Enum):
-    def __new__(cls, service_number, description, service_class):
-        obj = bytes.__new__(cls, [service_number])
-        obj._value_ = service_number
-        obj.description = description
-        obj.create = service_class
-        return obj
-
-    REQUEST_VERIFICATION = (1, "Request verification", RequestVerification)
-    DEVICE_ACCESS = (2, "Device access", None)
-    HOUSEKEEPING = (3, "Housekeeping", Housekeeping)
-    PARAMETER_STATISTICS_REPORTING = (4, "Parameter statistics reporting", None)
-    EVENT_REPORTING = (5, "Event reporting", EventReporting)
-    MEMORY_MANAGEMENT = (6, "Memory management", None)
-    FUNCTION_MANAGEMENT = (8, "Function Management", FunctionManagement)
-    TIME_MANAGEMENT = (9, "Time Management", None)
-    TIME_BASED_SCHEDULING = (11, "Time-based scheduling", None)
-    ONBOARD_MONITORING = (12, "On-board monitoring", None)
-    LARGE_PACKET_TRANSFER = (13, "Large packet transfer", None)
-    REALTIME_FORWARDING_CONTROL = (14, "Real-time forwarding control", None)
-    ONBOARD_STORAGE_AND_RETRIEVAL = (15, "On-board storage and retrieval", None)
-    TEST = (17, "Test", Test)
-    ONBOARD_CONTROL_PROCEDURES = (18, "On-board control procedures", None)
-    EVENT_ACTION = (19, "Event-action", None)
-    ONBOARD_PARAMETER_MANAGEMENT = (20, "On-board parameter management", None)
-    REQUEST_SEQUENCING = (21, "Request sequencing", None)
-    POSITION_BASED_SCHEDULING = (22, "Position-based scheduling", None)
-    FILE_MANAGEMENT = (23, "File management", None)
-
-    def __str__(self):
-        return self.description
+# Aliases
+PusService1 = RequestVerification
+PusService3 = Housekeeping
+PusService5 = EventReporting
+PusService8 = FunctionManagement
+PusService17 = Test
--- puslib/services/service.py
+++ puslib/services/service.py
@@ -6,6 +6,38 @@ from puslib.policy import PusPolicy
 from .error_codes import CommonErrorCode
 
 
+class PusServiceType(bytes, Enum):
+    def __new__(cls, service_number, description):
+        obj = bytes.__new__(cls, [service_number])
+        obj._value_ = service_number
+        obj.description = description
+        return obj
+
+    REQUEST_VERIFICATION = (1, "Request verification")
+    DEVICE_ACCESS = (2, "Device access")
+    HOUSEKEEPING = (3, "Housekeeping")
+    PARAMETER_STATISTICS_REPORTING = (4, "Parameter statistics reporting")
+    EVENT_REPORTING = (5, "Event reporting")
+    MEMORY_MANAGEMENT = (6, "Memory management")
+    FUNCTION_MANAGEMENT = (8, "Function Management")
+    TIME_MANAGEMENT = (9, "Time Management")
+    TIME_BASED_SCHEDULING = (11, "Time-based scheduling")
+    ONBOARD_MONITORING = (12, "On-board monitoring")
+    LARGE_PACKET_TRANSFER = (13, "Large packet transfer")
+    REALTIME_FORWARDING_CONTROL = (14, "Real-time forwarding control")
+    ONBOARD_STORAGE_AND_RETRIEVAL = (15, "On-board storage and retrieval")
+    TEST = (17, "Test")
+    ONBOARD_CONTROL_PROCEDURES = (18, "On-board control procedures")
+    EVENT_ACTION = (19, "Event-action")
+    ONBOARD_PARAMETER_MANAGEMENT = (20, "On-board parameter management")
+    REQUEST_SEQUENCING = (21, "Request sequencing")
+    POSITION_BASED_SCHEDULING = (22, "Position-based scheduling")
+    FILE_MANAGEMENT = (23, "File management")
+
+    def __str__(self):
+        return self.description
+
+
 class PusService:
     pus_policy = PusPolicy
 
@@ -19,11 +51,15 @@ class PusService:
 
     @property
     def service(self):
-        return self._service_type
+        return self._service_type.value
 
     @property
     def name(self):
         return self._service_type.name
+
+    @property
+    def description(self):
+        return self._service_type.description
 
     def enqueue(self, tc_packet):
         if tc_packet.apid == self._ident.apid and tc_packet.service == self.service and tc_packet.subservice in self._subservices:
