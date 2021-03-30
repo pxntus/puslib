0a9f4eab22ce0fd40e1f9868fb77ae690a4ddfd2
pxntus <pontusnyman@gmail.com>
2021-03-30 20:48:20 +0200

    Fixed sequence count generator.

--- puslib/ident.py
+++ puslib/ident.py
@@ -1,14 +1,19 @@
+def _seq_count_generator():
+    seq_count = -1
+    max_count = 2 ** 14
+    while True:
+        seq_count = (seq_count + 1) % max_count
+        yield seq_count
+
+
 class PusIdent:
     def __init__(self, apid):
         self._apid = apid
+        self._seq_count_generator = _seq_count_generator()
 
     @property
     def apid(self):
         return self._apid
 
-    def next_seq_count(self):
-        seq_count = -1
-        max_count = 2 ** 14
-        while True:
-            seq_count = (seq_count + 1) % max_count
-            yield seq_count
+    def seq_count(self):
+        return next(self._seq_count_generator)
