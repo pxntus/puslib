b7523b4be36396216f937003018faa1f09cfb7a7
pxntus <pontusnyman@gmail.com>
2021-04-01 19:25:14 +0200

    First implementation of application process.

---
+++ puslib/process.py
@@ -0,0 +1,65 @@
+from enum import IntEnum
+
+from . import services
+from .ident import PusIdent
+from .exceptions import TcPacketRoutingError
+from .services.service import PusServiceType
+
+
+def periodic(scheduler, interval, priority, action, actionargs=()):
+    scheduler.enter(interval, priority, periodic, (scheduler, interval, priority, action, actionargs))
+    action(*actionargs)
+
+
+class Priority(IntEnum):
+    HIGH = 1
+    NORMAL = 2
+    LOW = 3
+
+
+class Process:
+    def __init__(self, apid, tm_distributor, scheduler, housekeeping=False, event_reporting=False, function_management=False):
+
+        self._ident = PusIdent(apid)
+        self._tm_distributor = tm_distributor
+        self._scheduler = scheduler
+
+        self._pus_services = {}
+        self._pus_service_1 = services.RequestVerification(self._ident, tm_distributor)
+        self._pus_services[1] = self._pus_service_1
+        if housekeeping:
+            self._pus_service_3 = services.Housekeeping(self._ident, self._pus_service_1, tm_distributor)
+            self._pus_services[3] = self._pus_service_3
+        if event_reporting:
+            self._pus_service_5 = services.EventReporting(self._ident, self._pus_service_1, tm_distributor)
+            self._pus_services[5] = self._pus_service_5
+        if function_management:
+            self._pus_service_8 = services.FunctionManagement(self._ident, self._pus_service_1, tm_distributor)
+            self._pus_services[8] = self._pus_service_8
+
+        self._actions = {}
+
+    @property
+    def apid(self):
+        return self._ident.apid
+
+    def forward(self, tc_packet):
+        if tc_packet.apid != self.apid:
+            raise TcPacketRoutingError()
+        if tc_packet.service not in self._pus_services:
+            raise TcPacketRoutingError(f"No PUS service {tc_packet.service} in application process (APID {tc_packet.apid})")
+        self._pus_services[tc_packet.service].enqueue(tc_packet)
+        self._pus_services[tc_packet.service].process()
+
+    def action(self, interval, priority=Priority.NORMAL):
+        def add_action(func):
+            self._actions[func.__name__] = func
+            periodic(self._scheduler, interval, priority, func)
+        return add_action
+
+    def function(self, fid):
+        def add_function(func):
+            if PusServiceType.FUNCTION_MANAGEMENT.value not in self._pus_services:
+                raise RuntimeError("Process has no function management service")
+            self._pus_service_8.add(func, fid)
+        return add_function
