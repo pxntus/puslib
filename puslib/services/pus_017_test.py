76a5912c192e44220230c8b603cca635f42a9907
pxntus <pontusnyman@gmail.com>
2021-04-01 22:50:15 +0200

    Fixed PUS service 17.

--- puslib/services/pus_017_test.py
+++ puslib/services/pus_017_test.py
@@ -1,19 +1,20 @@
-from datetime import datetime
-
+from puslib import get_pus_policy
 from .service import PusService, PusServiceType
 
 
 class Test(PusService):
     def __init__(self, ident, pus_service_1, tm_distributor):
-        super().__init__(PusServiceType.TEST)
+        super().__init__(PusServiceType.TEST, ident, pus_service_1, tm_distributor)
         super()._register_sub_service(1, self.connection_test)
 
     def connection_test(self, packet):
-        report = Test.pus_policy.create_tm_packet(
+        time = get_pus_policy().CucTime()
+        report = get_pus_policy().PusTmPacket(
             apid=self._ident.apid,
-            seq_count=self._ident.next_seq_count(),
+            seq_count=self._ident.seq_count(),
             service_type=self._service_type.value,
             service_subtype=2,
-            time=Test.pus_policy.time().from_datetime(datetime.utcnow())
+            time=time
         )
-        return True, report
+        self._tm_distributor.send(report)
+        return True
