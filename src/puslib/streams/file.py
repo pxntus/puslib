7927b0b7049ffd901578aeed5fec23098059f78f
Pontus Nyman <pontus.nyman@ohb-sweden.se>
2023-09-08 17:44:01 +0200

    File input fix.

--- src/puslib/streams/file.py
+++ src/puslib/streams/file.py
@@ -5,8 +5,10 @@ from .stream import InputStream
 
 
 class FileInput(InputStream):
-    def __init__(self, archive_file, other_headers_size=0, validate_pec=True):
+    def __init__(self, archive_file, has_type_counter_field=True, has_destination_field=True, other_headers_size=0, validate_pec=True):
         self._input = Path(archive_file)
+        self._has_type_counter_field = has_type_counter_field
+        self._has_destination_field = has_destination_field
         self._other_headers_size = other_headers_size
         self._validate_pec = validate_pec
 
@@ -17,13 +19,13 @@ class FileInput(InputStream):
         offset = 0
         while offset < len(data):
             offset += self._other_headers_size
-            packet_length, packet = get_policy().PusTcPacket.deserialize(data[offset:], validate_fields=False, validate_pec=self._validate_pec)
-            offset += packet_length
+            packet = get_policy().PusTmPacket().deserialize(data[offset:], cuc_time=get_policy().CucTime(), has_type_counter_field=self._has_type_counter_field, has_destination_field=self._has_destination_field, validate_fields=False, validate_pec=self._validate_pec)
+            offset += len(packet)
             yield packet
 
     def read(self, offset=0):
         with open(self._input, 'rb') as f:
             content = f.read()
         data = memoryview(content)
-        _, packet = get_policy().PusTcPacket.deserialize(data[self._other_headers_size + offset:], validate_fields=False, validate_pec=False)
+        _, packet = get_policy().PusTmPacket.deserialize(data[self._other_headers_size + offset:], validate_fields=False, validate_pec=False)
         return packet
